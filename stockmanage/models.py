14ee8d839e8ae4ffa265802ad04d04158b3c4ff4
phiree <phiree@gmail.com>
2014-02-11 01:02:23 +0800

    unittest of Product _getName------docs

--- stockmanage/models.py
+++ stockmanage/models.py
@@ -24,7 +24,17 @@ class Product(models.Model):
     SyncTime=DateTimeField()
     ProductCode=CharField(max_length=255)
     ModelNumber_Original=CharField(max_length=255)
-    
+   
+    def GetName(self,language):
+        languageset=self.productlanguage_set.all()
+        matches=[x for x in languageset if x.Language==language]
+        if len(matches)==0:
+            raise Exception('product {0} has no  language version, its invalid'.format(self.id))
+        elif len(matches)>1:
+            raise Exception('product  has more than one ('+self.id+') language versions, it is invalid')    
+        else:
+            return matches[0].Name
+        
     class Meta:
         db_table="product"
     def __str__(self):
@@ -40,11 +50,17 @@ class Productlanguage(models.Model):
     ProductParameters=TextField()
     Unit=CharField(max_length=255)
     Language=CharField(max_length=255)
-    product=ForeignKey(Product)
+    theproduct=ForeignKey(Product)
     def __str__(self):
         return self.Name
     class Meta:
         db_table='productlanguage'
+
+class ProductStock(models.Model):
+    Qty=IntegerField(default=0)
+    theproduct=ForeignKey(Product)
+
+    
 
 
 
--- stockmanage/tests.py
+++ stockmanage/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
-
+from random import choice
+from autofixture import  AutoFixture,generators
+from stockmanage.models import  Product,Productlanguage
 # Create your tests here.
+class ProductMethodTests(TestCase):
+    def testGetnameWithMoreThanOneLanguageVersion(self):
+        fixtureProduct=AutoFixture(Product)
+        theproduct=fixtureProduct.create(1)[0]
+        fixtureProductlanguage=AutoFixture(Productlanguage,
+                                           field_values={
+                                                         'Language':'en',
+                                                         'theproduct':theproduct
+                                                         }
+                                           ,generate_fk=True)
+        theproductlanguage=fixtureProductlanguage.create(11)
+        self.assertRaisesRegex(Exception, '.*',)        
+        
+    def testGetnameWithOneLanguageVersion(self):
+        fixtureProduct=AutoFixture(Product)
+        theproduct=fixtureProduct.create(1)[0]
+        fixtureProductlanguage=AutoFixture(Productlanguage,
+                                           field_values={
+                                                         'Language':'en',
+                                                         'theproduct':theproduct
+                                                         }
+                                           ,generate_fk=True)
+        theproductlanguage=fixtureProductlanguage.create(1)
+        self.assertTrue(theproduct.GetName('en'))
+        
+    def testGetnameWithNoLanguageVersion(self):
+        fixtureProduct=AutoFixture(Product)
+        theproduct=fixtureProduct.create(1)[0]
+       
+        self.assertRaisesRegex(Exception, '.*',)    
+        
+        
