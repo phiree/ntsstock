28bab0e8831104a2c583f51e871687434bca6a50
phiree <phiree@gmail.com>
2014-02-20 01:00:54 +0800

    Revert "test ssl error fixting"
    This reverts commit e300fc4f28d180d1761ef6a7e5de7d00429a531e.

--- rebuild.bat
+++ rebuild.bat
@@ -1,4 +1,3 @@
 python manage.py sqlclear app1 app2 appN | sed -n "2,$p" | sed -n "$ !p" | sed "s/";/" CASCADE;/" | sed -e "1s/^/BEGIN;/" -e "$s/$/COMMIT;/" | python manage.py dbshell
 python manage.py syncdb
-
-pause  
+pause
