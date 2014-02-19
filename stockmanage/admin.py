41fc43fad7f61211895c4d4649145c829be3b4b4
phiree <phiree@gmail.com>
2014-02-19 16:15:49 +0800

    override the stocklocation admin listpage

--- stockmanage/admin.py
+++ stockmanage/admin.py
@@ -10,6 +10,15 @@ class ProductLanguageInLine(admin.TabularInline):
 class ProductAdmin(admin.ModelAdmin):
     inlines=[ProductLanguageInLine]
 
+class StockLocationAdmin(admin.ModelAdmin):
+    def changelist_view(self, request, extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['all_top_locations']=StockLocation.objects.filter(ParentLocation=None)
+        return super(StockLocationAdmin, self).changelist_view(request,extra_context=extra_context)
+
+class StockBillAdmin(admin.ModelAdmin):
+    #fields=('BillTime','BillType','TotalAmount','TotalKinds','BillState','StaffName','Memo') 
+    list_display =('BillTime','BillType','TotalAmount','TotalKinds','BillState','StaffName','Memo') 
 admin.site.register(Product,ProductAdmin)
-admin.site.register(StockLocation)
-admin.site.register(StockBill)
+admin.site.register(StockLocation,StockLocationAdmin)
+admin.site.register(StockBill,StockBillAdmin)
