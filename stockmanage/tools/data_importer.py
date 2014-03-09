3895323ce660498ad1484f30410848c4115e2fa5
phiree <phiree@gmail.com>
2014-03-09 15:42:51 +0800

    hh

--- stockmanage/tools/data_importer.py
+++ stockmanage/tools/data_importer.py
@@ -1,4 +1,4 @@
-from stockmanage.models import Product
+from stockmanage.models import Product,StockBill,StockBillDetail,ProductStock
 from stockmanage.tools.read_excel_to_list import excel_reader
 class importer:
     def __init__(self,excel_file_path):
@@ -6,26 +6,41 @@ class importer:
     
     def save_to_db(self):
         bookdata=excel_reader(excel_file_path).read()
+        #create a import bill for the init
         for sheet in bookdata['sheetsdata']:
             for row in sheet['rowlist']:
-                
+                save_single(row)
         
-    def save_single(cell_values):
+    def save_single(row):
         '''save list to database'''
+        #save product info
         product,created=Product.objects.get_or_create(
-                                      SerialNo=cell_values[0],
-                                      Code_Original=cell_values[1],
-                                      Code_Database=cell_values[2],
+                                      SerialNo=row[0],
+                                      Code_Original=row[1],
+                                      Code_Database=row[2],
                                       NTSCode='',
-                                      Name=cell_values[3],
-                                      ModelNumber=cell_values[4],
-                                      Specification=cell_values[5],
-                                      Material=cell_values[6],
-                                      Price=cell_values[7],
-                                      Unit=cell_values[8],
-                                      SupplierCode=cell_values[9],
-                                      State=cell_values[10]
+                                      Name=row[3],
+                                      ModelNumber=row[4],
+                                      Specification=row[5],
+                                      Material=row[6],
+                                      Price=row[7],
+                                      Unit=row[8],
+                                      SupplierCode=row[9],
+                                      State=row[10]
                                       )
+        #save productstock
+        quantity=int(row[11] if row[11].strip() else row[10])
+        locationCode=row[13]
+        location=StockLocation.objects.get(LocationCode=locationCode)
+        ProductStock(Quantity=quantity,theproduct=product,location=location).save()
+        
+    def parse_location(self,location_code):
+        
+        
+                  
+        
+            
+        
         
             
         
